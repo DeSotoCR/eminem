c114df4c930c313e49f45d134cf288c9a9e03493
aleo2 <aleo2@AleSystem>
2023-05-30 21:27:53 -0600

    Inicio Tarea 02

---
+++ src/test/java/com/eminem/EminemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EminemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
